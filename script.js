15b1a2c0b0718d021d69383e385a10847229f9fd
Rounak Datta <rounakdatta12@gmail.com>
2022-06-30 00:18:15 +0530

    Fix navigation bugs, add icon

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const PROFILE_PANEL_CLASS = "css-1dbjc4n r-1habvwh r-18u37iz r-1w6e6rj r-1wtj0ep"
 const HEADER_PANEL_CLASS = "css-1dbjc4n r-1p0dtai r-1mlwlqe r-1d2f490 r-1udh08x r-u8s1d r-zchlnj r-ipm5af r-417010"
+const BLANK_HEADER_PANEL_CLASS = "r-1p0dtai r-1pi2tsx r-1d2f490 r-u8s1d r-ipm5af r-13qz1uu"
 const SEARCH_INPUT_ATTRIBUTE = "SearchBox_Search_Input_label"
 
 // transparent background for search button
-const DESIRED_BACKGROUND_COLOR = "rgba(0, 0, 0, 0)"
+const WHITE_BACKGROUND_COLOR = "rgba(0, 0, 0, 0)"
+const TRANSPARENT_BACKGROUND_COLOR = "rgb(255 255 255 / 0%)"
 
 // function to identify a valid twitter profile page
 function getProfileNameIfValidURL(url) {
@@ -43,29 +45,47 @@ function styleAndFunctionalizeSearchButton(searchButton, username) {
     searchButton.children[0].children[0].replaceWith(searchSVG)
 
     // set background color
-    searchButton.style.backgroundColor = DESIRED_BACKGROUND_COLOR
+    searchButton.style.backgroundColor = WHITE_BACKGROUND_COLOR
+
+    // nevertheless this is required only in case of self-profiles where there is a white border
+    searchButton.style.borderColor = TRANSPARENT_BACKGROUND_COLOR
 
     // remove existing onClick actions
     searchButton.removeAttribute('data-testid')
 
     // build the search URL and take there upon click
     const searchPageURL = getSearchPageURL(username)
-    searchButton.addEventListener("click", function(event){
+    searchButton.addEventListener("click", function (event) {
         event.preventDefault()
         window.location.assign(searchPageURL)
-      });
+    });
 }
 
 function cloneProfilePanelToHeader(username) {
     let profilePanelLookalikes = document.getElementsByClassName(PROFILE_PANEL_CLASS)
-    // TODO: add length check
+
+    if (profilePanelLookalikes.length == 0) {
+        return null
+    }
+
     const profilePanelClone = profilePanelLookalikes[0].cloneNode(true)
 
     let headerPhotoUrl = getHeaderPhotoRelativeURL(username)
     let baseHeaderPanelIdentifier = `[href="${headerPhotoUrl}"]`
 
+
     // locate the header panel base div, and append the profile panel clone to it
-    let headerPanel = document.querySelectorAll(baseHeaderPanelIdentifier)[0].getElementsByClassName(HEADER_PANEL_CLASS)[0]
+    let headerArea = document.querySelectorAll(baseHeaderPanelIdentifier)
+    let headerPanel = null;
+
+    if (headerArea.length == 0) {
+        // case of blank header-photo profiles
+        headerArea = document.querySelectorAll('[aria-label="Home timeline"]')
+        headerPanel = headerArea[0].getElementsByClassName(BLANK_HEADER_PANEL_CLASS)[0]
+    } else {
+        headerPanel = headerArea[0].getElementsByClassName(HEADER_PANEL_CLASS)[0]
+    }
+
     headerPanel.appendChild(profilePanelClone)
 
     return headerPanel
@@ -78,7 +98,11 @@ function transformItemsInHeaderPanel(headerPanel, username) {
     headerActionPanel.children[0].style.visibility = "hidden"
 
     let actionButtons = headerActionPanel.children[1].querySelectorAll('[role="button"]')
-    // TODO: check that length is > 1
+
+    // handle self-profile case
+    if (actionButtons.length == 0) {
+        actionButtons = headerActionPanel.children[1].querySelectorAll('[data-testid="editProfileButton"]')
+    }
 
     // mark all but last action button as completely hidden
     for (let i = 0; i < actionButtons.length - 1; i++) {
@@ -94,14 +118,17 @@ function act(currentURL) {
     // null check
 
     let headerPanel = cloneProfilePanelToHeader(username)
+    if (headerPanel == null) {
+        return
+    }
     transformItemsInHeaderPanel(headerPanel, username)
 }
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (request.message === 'BUILD_TWITTER_PROFILE_SEARCH') {
-        window.setTimeout(function() {
+    function (request, sender, sendResponse) {
+        if (request.message === 'BUILD_TWITTER_PROFILE_SEARCH') {
+            window.setTimeout(function () {
                 act(request.url)
             }, 2000) // TODO: trigger this only after page completely loads
-      }
-  });
+        }
+    });
